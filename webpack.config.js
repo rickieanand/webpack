aa0fb5c57001baed18db71dd5f845f28cbcc237a
Rickie Anand <rickie.anand@bookmyshow.com>
2017-03-24 16:34:09 +0530

    implement htmlwebpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,15 @@
+let HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
     entry: './index.js',
     output: {
         filename: 'public/index.js'
-    }
+    },
+    devServer: {
+        contentBase: './public'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html'
+        })
+    ]
 }
