777182b6991e7f6b4c3349fbbae10cfd16b46e3a
Rickie Anand <rickie.anand@bookmyshow.com>
2017-03-24 17:38:57 +0530

    successfully installed 2 chunk app

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 console.log('Hello')
+document.getElementById('app').addEventListener('click', function(e){
+    require.ensure([], function(require){
+        require('./app.js')
+    }, 'appView')
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,11 @@
 let HtmlWebpackPlugin = require('html-webpack-plugin')
+let path = require('path')
 module.exports = {
     entry: './index.js',
     output: {
-        filename: 'public/index.js'
+        path: path.join(__dirname, 'public'),
+        filename: 'index.js',
+        chunkFilename: '[name].chunk.js'
     },
     devServer: {
         contentBase: './public'
